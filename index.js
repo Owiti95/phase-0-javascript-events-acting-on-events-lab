a174463a66ac30b6c7833f19952337e4daf5478d
hannington owiti <hanningtonowiti@gmail.com>
2024-07-07 19:43:32 +0300

    dodger

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+
+  //move dodger to the right
+
+  //the function to move dodger to the right
+
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    const dodgerWidth = dodger.clientWidth; //gets dodger element's width
+    if (left < window.innerWidth - dodgerWidth) {
+        dodger.style.left = `${left + 1}px`;
+    }
+  }
+
+//the event listener
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();}
+});
